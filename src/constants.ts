7937815f128c0bd3ad560382eedca15aaa104934
Yael Molinero <y.molinero20@gmail.com>
2025-12-14 21:27:34 -0600

    First commit

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const navigationLinks = [
+  { name: "Inicio", href: "/" },
+  { name: "Nosotros", href: "/nosotros" },
+  { name: "Producto", href: "/producto" },
+  { name: "Contacto", href: "/contacto" },
+] as const;
